534169f2f126c1672d2a97d852efd55cd5b58e1f
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-02-25 12:22:03 -0600

    Feat - New Feature. Adding 1D implementation of the Kalman filter

---
+++ KalmanFilter1D.hpp
@@ -0,0 +1,13 @@
+//
+//  KalmanFilter1D.hpp
+//  
+//
+//  Created by Biplab kumar das on 2/25/18.
+//
+
+#ifndef KalmanFilter1D_hpp
+#define KalmanFilter1D_hpp
+
+#include <stdio.h>
+
+#endif /* KalmanFilter1D_hpp */
